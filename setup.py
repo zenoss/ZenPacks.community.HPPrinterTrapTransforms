cdaadab9ccb4035f33840cd19e4e7a1d8c61dfb8
Daniel Robbins <drobbins@funtoo.org>
2012-05-07 16:45:09 +0000

    GPLv2 licensing fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 NAME = "ZenPacks.community.HPPrinterTrapTransforms"
 VERSION = "1.0"
 AUTHOR = "John Field"
-LICENSE = ""
+LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.HPPrinterTrapTransforms']
 INSTALL_REQUIRES = []
